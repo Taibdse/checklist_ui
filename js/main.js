4c9482988b9d4754730379e4802206fbbee372e0
taibd1 <ductai.bui@aia.com>
2021-09-08 11:05:10 +0700

    update css/js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
+
+  $(document).on('click', function (e) {
+    var container = $("#sidebar");
+    var sidebarCollapse = $('#sidebarCollapse');
+    if (!container.hasClass('active') || container.css('position') !== 'fixed') return;
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0 && sidebarCollapse.has(e.target).length === 0 && !sidebarCollapse.is(e.target)) {
+      container.removeClass("active");
+    }
+  });
+
   $("#sidebarCollapse").on("click", function () {
     $("#sidebar").toggleClass("active");
     // $(this).toggleClass("active");
   });
 });
+
